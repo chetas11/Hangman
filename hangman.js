e46303598dbfc72424606ec48374ceecf5d36497
chetas11 <cbirmole@gmail.com>
2020-08-12 20:21:51 +0530

    firstCommit

---
+++ hangman.js
@@ -0,0 +1,39 @@
+var programmingLaunguages = [
+    "python",
+	"javascript",
+	"mongodb",
+	"json",
+	"java",
+	"html",
+	"css",
+	"c",
+	"csharp",
+	"golang",
+	"kotlin",
+	"php",
+	"sql",
+	"ruby"
+]
+
+let answers = '';
+let maxwrong = 7;
+let mistakes = 0;
+let guessed = [];
+
+function randomWord(){
+    answers = programmingLaunguages[Math.floor(Math.random()*programmingLaunguages.length)];
+}
+
+function generateButtons(){
+    let buttonsHTML = "abcdefghijklmnopqrestuvwxyz".split('').map(letter =>
+        `
+        <button
+        class = "btn btn-lg btn-primary m-2" id = '` + letter + `' onClick ="handleGuess('` + letter + `')">
+        ` + letter + `
+        </button>
+        `).join("");
+    document.getElementById("keyboard").innerHTML = buttonsHTML;
+}
+    document.getElementById("maxwrong").innerHTML= maxwrong;
+randomWord();
+generateButtons();
